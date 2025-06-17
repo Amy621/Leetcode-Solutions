0497b93c742b85d0d71ee37d1b8c28d32a2f4aae
Amy Hakim <129816655+Amy621@users.noreply.github.com>
2025-06-17 16:28:34 -0400

    Time: 0 ms (100%), Space: 12.4 MB (9.03%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2,int n) {
+         int j = 0;
+         int dont = (m+n) - n;
+         for(int i = 0; i < n; i++){
+           // cout << "In nums1 we are at: " << nums1[j] << " which is iterator: " << j << " and in nums2 we are at: " << nums2[i] << "\n";
+            while(nums1[j] < nums2[i]){
+             //   cout << nums1[j] << " is less than " << nums2[i] << "\n";
+                if(nums1[j] == 0 && j >= dont) break;
+                j++;
+              //  cout << "iterator j is now: " << j << "\n";
+            }
+            nums1.insert(nums1.begin() + j, nums2[i]);
+          //  cout << "Inserting: " << nums2[i] << " after " << nums1[j] << "\n";
+            dont++;
+            nums1.pop_back();
+
+         }
+    }
+};
+// 
