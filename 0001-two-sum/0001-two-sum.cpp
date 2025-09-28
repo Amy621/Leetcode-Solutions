8ecf63cbdaf7ad0ed4fb932bdb25d796766a89fa
Amy Hakim <129816655+Amy621@users.noreply.github.com>
2025-09-28 15:52:01 -0400

    Time: 0 ms (100%), Space: 14.8 MB (36.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-// go through list, insert into map
-// go through list second time, if target - cur is in the map, return true
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, unordered_set<int>> mp;
+        unordered_map<int, int> mp;
         int len = nums.size();
         vector<int> ans;
-        for(int i = 0; i < len; i++) mp[nums[i]].insert(i);
-        
         for(int i = 0; i < len; i++){
-            mp[nums[i]].erase(i);
-            int cur = target - nums[i];
-            if(mp[cur].size() >= 1){
+
+            int v = target - nums[i];
+
+            if(mp.find(v) != mp.end()){
+                ans.push_back(mp[v]);
                 ans.push_back(i);
-                int ret = *mp[cur].begin();
-                ans.push_back(ret);
-                break;
+                return ans;
             }
-            mp[nums[i]].insert(i);
+
+            mp[nums[i]] = i;
+
+            
         }
 
         return ans;
