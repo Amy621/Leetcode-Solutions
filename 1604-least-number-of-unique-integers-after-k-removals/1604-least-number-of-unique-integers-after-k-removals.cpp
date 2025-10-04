d50b0a94710847d94305d4ebbd54e981199f059f
Amy Hakim <129816655+Amy621@users.noreply.github.com>
2025-10-04 14:13:37 -0400

    Time: 103 ms (87.29%), Space: 65.7 MB (91.38%) - LeetHub

--- 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.cpp
@@ -7,22 +7,37 @@ public:
 
         for(auto &v : arr) mp[v]++;
 
-        vector<vector<int>> freqs(len + 1);
+        vector<int> freqs(len + 1);
 
-        for(auto &p : mp) freqs[p.second].push_back(p.first);
-       
+        for(auto &p : mp) freqs[p.second]++;
+
+        // k = 5
+        // k = 2
+        // freq : 3,  (1 1 1, 2 2, 2)
+        // k / i
+        // 5 / 3 = 1 , remove 1 value of freq 3, (k - (5/ 3) * i)
+        // 6 / 3 = 2, remove 2
+        int cnt = 0;
         for(int i = 1; i < freqs.size(); i++){
-            if(i > k) break;
-            for(auto &v : freqs[i]){
-                if (k == 0) break;
-                if(k - i < 0) break;
-                k -= i;
-                mp.erase(v);
-            
-            }
+            if(i > k || k == 0) break;
+            if(freqs[i] == 0) continue;
+
+            int rems = k / i;
+            if(rems == 0) continue;
+            int amt = freqs[i];
+
+            if(rems > amt) rems = amt;
+
+            if(k - (rems * i) < 0) break;
+            cnt += (rems);
+            k -= (rems * i);
+
+
+
+
         }
 
         
-        return mp.size();
+        return mp.size() - cnt;
     }
 };
