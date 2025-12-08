0c778409fb19d35ef02bb791216397b238c9c665
Amy Hakim <129816655+amyhakim@users.noreply.github.com>
2025-12-08 16:55:25 -0500

    Time: 52 ms (96.76%), Space: 191.5 MB (5.45%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,38 +1,43 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-
-// edge cases: contains one 0
-//  [2, 3, 6, 6, 12, 4]
-// length of input is not divisible by 2
-// we need to match smallest to largest
     vector<int> findOriginalArray(vector<int>& changed) {
-
-        sort(changed.begin(), changed.end());
-        unordered_map<int, int> mp;
-        int len = changed.size();
-         
-        for(auto &v : changed) mp[v]++;
         
-        vector<int> ans;
-        if(len % 2 != 0) return ans;
-        for(auto &v : changed){
-            if(mp[v] == 0) continue; // we already matched this guy
-            if(mp[v*2] != 0 && ans.size() < len / 2){
-                        if(v * 2 == 0 && mp[v*2] < 2) continue;
-                  
-                        ans.push_back(v);
-                        mp[v*2]--;
-                        mp[v]--; // when we match, remove both instances
-                    
-                }
+        int n = changed.size();
+        if (n % 2 != 0) return {};   
+        
+        int mid = n / 2;
+        vector<int> res(mid);
+        
+
+        const int MAXV = 100000;
+        vector<int> freq(MAXV + 1, 0);
+        
+        for (int x : changed) {
+            if (x > MAXV) return {};  
+            freq[x]++;
+        }
+        
+        int idx = 0;
+        
+     
+        for (int no = 0; no <= MAXV; no++) {
+            while (freq[no] > 0 && no * 2 <= MAXV && freq[no * 2] > 0) {
+                freq[no]--;
+                freq[no * 2]--;
+                res[idx++] = no;
             }
+        }
         
-
-
-        if(ans.size() != len / 2) ans.clear();
-        return ans;
-
+  
+        for (int i = 0; i <= MAXV; i++) {
+            if (freq[i] != 0) {
+                return {};
+            }
+        }
         
-        
+        return res;
     }
 };
